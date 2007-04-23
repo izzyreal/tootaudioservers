878dac8ccc3b885376c328286409cfd499e4d621
PaulJohnLeonard <PaulJohnLeonard@733547dc-9d2e-0410-8fe0-b399404d3dce>
2007-04-23 14:48:08 +0000

    Updated PriorityAudioServer from frinika code. Now has low priority watch dog.

--- src/com/frinika/toot/PriorityAudioServer.java
+++ src/com/frinika/toot/PriorityAudioServer.java
@@ -28,21 +28,71 @@ import com.frinika.priority.Priority;
 
 import uk.org.toot.audio.server.BasicAudioServer;
 
+/**
+ * 
+ * @author Paul J. Leonard
+ * @author Peter J. Salomonsen
+ *
+ */
 public abstract class PriorityAudioServer extends BasicAudioServer  {
 
-	private static boolean isLinux =
-		System.getProperty("os.name").equals("Linux");
-	
 	private int priorityRequested = -1;
 	private int priority = 0;
 
+	/**
+	 * The watchDogTimestamp should be as close to system.currentTimeMillis() as possible. If the difference is more than 100ms then the priority 
+	 * thread should go to sleep as long as the watchDogTimestamp differs.
+	 */
+	private static long watchDogTimestamp;
+	/**
+	 * Create the watchdog thread - a low priority thread that updates the watchDogTimestamp every millisecond. If the realtime priority thread
+	 * starts occupying the system resources - the watchdog timestamp will not be updated. Thus the realtime priority thread should check
+	 * that the watchDogTimestamp does not differ more than 100ms from System.currentTimeMillis() - and if it does, the realtime thread should
+	 * sleep until the watchDogTimestamp is updated again.
+	 */
+	static
+	{
+	
+		Thread thr = new Thread()
+		{
+			public void run()
+			{
+				while(true)
+				{
+					watchDogTimestamp = System.currentTimeMillis();
+					try {
+						sleep(1);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}			
+				}
+			}
+		};
+		thr.setPriority(Thread.MIN_PRIORITY);
+		thr.start();
+	}
+	
 	public PriorityAudioServer() {
 	}
 
 	public void work() {
-	
+		/**
+		 * Sleep while System.currentTimeMillis differs more than 100 ms from watchDogTimestamp
+		 */
+		while((System.currentTimeMillis()-watchDogTimestamp)>100)
+		{
+			try {
+				System.out.println("PRIORITY THREAD WATCHDOG: System was blocked for at least "+(System.currentTimeMillis()-watchDogTimestamp)+" ms, freeing resources");
+				Thread.sleep(1);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
 		if (priorityRequested != -1) {
-			if (isLinux) {
+			if (System.getProperty("os.name").equals("Linux")) {
 				try {
 					int prio=priorityRequested;
 						if (prio > 0) {
